adadc8c9bebc3a077e3a6ca97f88ce9e1dd892c9
Keyur <keyur.satyadev@spinxwebdesign.com>
2013-09-04 14:32:46 +0530

    start using github

---
+++ SignalR/ChatHub.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SignalR
+{
+    public partial class ChatHub : System.Web.UI.Page
+    {
+        /// <summary>
+        /// summary of method
+        /// <Created_by></Created_by>
+        /// <Created_Date>04/09/2013</Created_Date>
+        /// <Class_Details></Class_Details>
+        /// <Modified_By></Modified_By>
+        /// <Modified_Date></Modified_Date>
+        /// <Change_Description></Change_Description>>
+        /// </summary> 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+      
+    }
+}
---
+++ SignalR/product.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace SignalR
+{
+    public partial class product : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                foreach (var items in Request.GetFriendlyUrlSegments())
+                {
+                    Response.Write(items);
+                    Response.Write("<br/>");
+                }
+   
+            }
+
+        }
+        
+    }
+}
