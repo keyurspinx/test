bd05f9a84e692e8d7800298a459de506a232f5fb
Keyur <keyur.satyadev@spinxwebdesign.com>
2013-09-04 14:33:27 +0530

    Update By Keyur

--- SignalR/ChatHub.aspx.cs
+++ SignalR/ChatHub.aspx.cs
@@ -11,7 +11,7 @@ namespace SignalR
     {
         /// <summary>
         /// summary of method
-        /// <Created_by></Created_by>
+        /// <Created_by>KMS</Created_by>
         /// <Created_Date>04/09/2013</Created_Date>
         /// <Class_Details></Class_Details>
         /// <Modified_By></Modified_By>
